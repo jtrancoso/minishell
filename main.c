a1619998da63b535374687f6e2ab05c69c5bdb36
Ignacio Soria garnacho <isoria-g@c2r8s6.42madrid.com>
2021-11-13 10:42:10 +0100

    Doing some norm in main.c, not finish

--- main.c
+++ main.c
@@ -3,35 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtrancos <jtrancos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: isoria-g <isoria-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 13:22:40 by jtrancos          #+#    #+#             */
-/*   Updated: 2021/11/12 14:09:29 by jtrancos         ###   ########.fr       */
+/*   Updated: 2021/11/13 10:41:32 by isoria-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void miraleaks()
+void	miraleaks()
 {
 	system("leaks minishell");
 }
-int main (int argv, char **argc, char **envp)
-{
-	t_list *list;
-	t_list *new;
-	t_env *env;
-	t_comm comm;
-	t_split split;
-	int i;
 
-	//atexit(miraleaks);
-	print_prompt(&comm);
-	comm.env_head = NULL;
-	char **split_env;
+void	check_env(t_comm *comm, char **envp)
+{
+	t_list	*new;
+	t_env	*env;
+	int		i;
+	char	**split_env;
+
 	i = 0;
+	comm->env_head = NULL;
 	if (!envp[i])
-		galactic_env(&comm);
+		galactic_env(comm);
 	i = 0;
 	while (envp[i])
 	{
@@ -39,23 +35,37 @@ int main (int argv, char **argc, char **envp)
 		env = ft_malloc(sizeof(t_env));
 		split_env = ft_split(envp[i], '=');
 		new->content = env;
-		((t_env*)new->content)->id = split_env[0];
-		((t_env*)new->content)->value = split_env[1];
-		ft_lstadd_back(&comm.env_head, new);
+		((t_env *)new->content)->id = split_env[0];
+		((t_env *)new->content)->value = split_env[1];
+		ft_lstadd_back(&comm->env_head, new);
 		free(split_env);
 		i++;
 	}
+}
+
+int	main(int argv, char **argc, char **envp)
+{
+	t_list	*list;
+	t_list	*new;
+	t_env	*env;
+	t_comm	comm;
+	t_split	split;
+	int		i;
+
+	//atexit(miraleaks);
+	print_prompt(&comm);
+	check_env(&comm, envp);
 	list = comm.env_head;
 	while (list)
 	{
-		if (ft_strncmp(((t_env*)list->content)->id, "USER", 4) == 0)
-			comm.user = ft_strdup(((t_env*)list->content)->value);
-		else if (ft_strncmp(((t_env*)list->content)->id, "SHLVL", 5) == 0)
-			((t_env*)list->content)->value = next_shlvl(((t_env*)list->content)->value);
+		if (ft_strncmp(((t_env *)list->content)->id, "USER", 4) == 0)
+			comm.user = ft_strdup(((t_env *)list->content)->value);
+		else if (ft_strncmp(((t_env *)list->content)->id, "SHLVL", 5) == 0)
+			((t_env *)list->content)->value = next_shlvl(((t_env *)list->content)->value);
 		list = list->next;
 	}
 	split.errorcode = 0;
-	int pars; //para gestionar el error de sintaxis y que no libere
+	int	pars;//para gestionar el error de sintaxis y que no libere
 	while (1)
 	{
 		signal(SIGINT, default_sigint);
@@ -71,11 +81,11 @@ int main (int argv, char **argc, char **envp)
 		i = 1;
 		while (list)
 		{
-			if (((t_comm*)list->content)->t_pipe == 0 || ((t_comm*)list->content)->t_semi == 0)
-				((t_comm*)list->content)->page = i;
-			if (((t_comm*)list->content)->t_pipe == 1 || ((t_comm*)list->content)->t_semi == 1)
+			if (((t_comm *)list->content)->t_pipe == 0 || ((t_comm *)list->content)->t_semi == 0)
+				((t_comm *)list->content)->page = i;
+			if (((t_comm *)list->content)->t_pipe == 1 || ((t_comm *)list->content)->t_semi == 1)
 			{
-				((t_comm*)list->content)->page = 0;
+				((t_comm *)list->content)->page = 0;
 				i++;
 			}
 			list = list->next;
@@ -91,15 +101,15 @@ int main (int argv, char **argc, char **envp)
 		list = comm.parse_head;
 		while (list)
 		{
-			if (((t_comm*)list->content)->t_word != NULL && list->next)
+			if (((t_comm *)list->content)->t_word != NULL && list->next)
 			{
-				if (((t_comm*)list->next->content)->t_pipe == 1)
-					((t_comm*)list->content)->post_pipe = 1;
+				if (((t_comm *)list->next->content)->t_pipe == 1)
+					((t_comm *)list->content)->post_pipe = 1;
 			}
-			if (((t_comm*)list->content)->t_pipe == 1)
+			if (((t_comm *)list->content)->t_pipe == 1)
 			{
-				if (((t_comm*)list->next->content)->t_word != NULL)
-					((t_comm*)list->next->content)->prev_pipe = 1;
+				if (((t_comm *)list->next->content)->t_word != NULL)
+					((t_comm *)list->next->content)->prev_pipe = 1;
 			}
 			list = list->next;
 		}
